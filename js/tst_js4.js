ec59cb7f03730db70b14261129679535a306967b
zaiaaa <gustavo.0801214@gmail.com>
2022-05-31 20:49:13 -0300

    update database

---
+++ js/tst_js4.js
@@ -0,0 +1,9 @@
+let lmt = 100
+let sld_atl = 100
+
+let r = (sld_atl < lmt) ? "Você ainda pode depositar dinheiro" : "Seu limite foi atingido.";
+
+
+alert(r)
+
+//let variável = (condição) ? //instruçao se true: // Instrução se false
---
+++ js/tst_js5.js
@@ -0,0 +1,19 @@
+let data_hora = new Date();
+let dia_semana = data_hora.getDay();
+switch(dia_semana){
+    case 0: alert("Domingo de descanso merecido.");
+        break
+    case 1: alert("Segunda, o dia mais triste da história");
+        break
+    case 2: alert("Terçou bbs");
+        break
+    case 3: alert("Já estamos no meio da semana! Quarta feira até que é legal");
+        break    
+    case 4: alert("Cheou a pré-sexta, ta quase acabando!");
+        break
+        case 5: alert("Chegou a sexta-feira, isso é sensacional!");
+        break
+    case 6: alert("Maravilha! Sábado é dia de fut");
+        break
+    default: alert("Que semana longa.");
+}
